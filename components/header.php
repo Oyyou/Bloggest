819a085d2d43e17b0b90746216a807ada7f0dfac
Niall Lewin <Lewin91@hotmail.com>
2021-08-01 12:59:09 +0100

    Styled the dropdown

--- components/header.php
+++ components/header.php
@@ -6,13 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bloggest</title>
-    <meta name="description" content="" />
+    <meta name="description" content="We have blogs" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/main.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <title>Document</title>
+    <title>Bloggest</title>
 </head>
 
 <body>
