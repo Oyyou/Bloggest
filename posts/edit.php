5808312a269644653a8ea28596f306776cdb9a9a
Niall Lewin <Lewin91@hotmail.com>
2021-08-05 20:53:21 +0100

    Edit page now saves images properly

--- posts/edit.php
+++ posts/edit.php
@@ -1,6 +1,6 @@
 <?php
 
-$uploadPath = "/uploads/";
+$uploadPath = $_SERVER['DOCUMENT_ROOT'] . "/uploads/";
 
 include("../components/header.php");
 include("../components/nav.php");
@@ -19,6 +19,7 @@ $componentList = array();
 
 if ($blogId) {
     include("../database.php");
+    include("../php-functions.php");
     $conn = getConnection();
     $qid = $blogId;
     $blog = getBlogById($conn, $blogId);
@@ -85,6 +86,25 @@ if (isset($_POST["title"]) && isset($_POST["shortDescription"]) && isset($blogId
 
     if (isset($_POST['submit'])) {
 
+        $allImagesSet = true;
+
+        if (isset($_FILES["images"])) {
+            foreach ($_FILES["images"]["tmp_name"] as $key => $tmp_name) {
+
+                $image = $_FILES["images"]["tmp_name"][$key];
+
+                if (empty($image)) {
+                    $allImagesSet = false;
+                } else {
+                    $image_size = getimagesize($image);
+
+                    if ($image_size === false) {
+                        $allImagesSet = false;
+                    }
+                }
+            }
+        }
+
         $query = "UPDATE Blogs SET title=?, shortDescription=?, tags=? WHERE id=?";
         $stmt = $conn->prepare($query);
 
@@ -96,9 +116,43 @@ if (isset($_POST["title"]) && isset($_POST["shortDescription"]) && isset($blogId
         $stmt->bind_param("sssi", $title, $shortDescription, $tags, $blogId);
 
         $stmt->execute();
+        $newImageNames = array();
+
+        if ($allImagesSet) {
+
+            if (isset($_FILES["images"])) {
+                foreach ($_FILES["images"]["tmp_name"] as $key => $tmp_name) {
+
+                    $file_name = $_FILES["images"]["name"][$key];
+                    $file_tmp = $_FILES["images"]["tmp_name"][$key];
+                    $image_size = getimagesize($_FILES["images"]["tmp_name"][$key]);
+                    $ext = pathinfo($file_name, PATHINFO_EXTENSION);
+                    $newFileName = generateRandomString() . "." . $ext;
+
+                    $target_file = $uploadPath . $newFileName;
+
+                    var_dump($file_tmp);
+                    var_dump($target_file);
+
+                    move_uploaded_file($file_tmp, $target_file);
+
+                    $newImageNames[$file_name] = $newFileName;
+
+                    $uploadOk = 1;
+                    if ($image_size !== false) {
+                        $uploadOk = 1;
+                    } else {
+                        echo "File is not an image.";
+                        $uploadOk = 0;
+                    }
+                }
+            }
+        } else {
+            echo "Not all images have a value..!";
+        }
 
         $usedUUIDs = array();
-        
+
         foreach ($_POST["components"] as $key => $component) {
             $componentObj = json_decode($component);
 
@@ -109,15 +163,25 @@ if (isset($_POST["title"]) && isset($_POST["shortDescription"]) && isset($blogId
                 // Update existing component
                 updateBlogComponent($conn, $dbComponent->id, $key, $componentObj->value);
             } else {
+
+                $value = $componentObj->value;
+
+                if (isset($newImageNames[$value])) {
+                    $value = $newImageNames[$value];
+                }
+
+                $value = str_replace(array("\n", "\r"), '', nl2br(htmlspecialchars($value)));
+
                 // Add new component
-                addBlogComponent($conn, $blogId, $componentObj->uuid, $key, $componentObj->type, $componentObj->value);
+                addBlogComponent($conn, $blogId, $componentObj->uuid, $key, $componentObj->type, $value);
             }
 
             array_push($usedUUIDs, $componentObj->uuid);
         }
 
         $dbComponents = getBlogComponents($conn, $blogId);
-        foreach ($dbComponents as $component) {            
+        foreach ($dbComponents as $component) {
+            continue;
             if (!in_array($component["uuid"], $usedUUIDs)) {
                 deleteBlogComponent($conn, $component["id"]);
             }
@@ -132,7 +196,7 @@ if (isset($_POST["title"]) && isset($_POST["shortDescription"]) && isset($blogId
 
 ?>
     <script type="text/javascript">
-        window.location = "/dashboard";
+        //window.location = "/dashboard";
     </script>
 <?php
 }
